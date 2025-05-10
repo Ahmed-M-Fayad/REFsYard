6afb1e7d4c5764d733424ea0d3310f36c67ea987
Ahmed Fayad <ahmedfayad390@gmail.com>
2025-05-10 23:47:50 +0300

    Re-organized the coding files

--- home.php
+++ home.php
@@ -37,7 +37,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REFsYard Bookstore</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="style.css?v=22">
+    <link rel="stylesheet" href="Stylesheets/style.css?v=22">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     
 </head>
